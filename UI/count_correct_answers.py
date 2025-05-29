ea4f6e843ac722ea495fed8276732e99b809cf81
salialavi <sa.alavi.b@gmail.com>
2025-05-29 15:20:37 -0400

    new data

--- UI/count_correct_answers.py
+++ UI/count_correct_answers.py
@@ -1,7 +1,7 @@
 import json
 
 # Load the JSON file
-participant = "Subject 7"
+participant = "Subject 8"
 with open('experiment_data/' + participant+ '/answers.json', 'r') as file:
     data = json.load(file)
 
